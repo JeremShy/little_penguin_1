23d76571527ce402d4e237d24e5317f135a41641
neo <neo@jcamhi.42>
2018-02-05 17:48:19 +0100

    over

--- ex01/main.c
+++ ex01/main.c
@@ -2,6 +2,10 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 
+MODULE_AUTHOR("jcamhi");
+MODULE_DESCRIPTION("my module\n");
+MODULE_LICENSE("GPL");
+
 int __init _ft_init(void)
 {
 	pr_debug("Hello world !\n");
